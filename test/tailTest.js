94606251e8b14fc9345e3e512734642a91ab087c
Shujie Ma <shujie1st@gmail.com>
2023-02-15 15:30:39 -0500

    Refactor code with Mocha and Chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,24 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
-
-// TEST CASE 1
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-// TEST CASE 2
-const numbers = tail([5, 6, 7, 8, 9]);
-assertEqual(numbers.length, 4);
-assertEqual(numbers[2], 8);
-assertEqual(numbers[0], 6);
-
-// TEST CASE 3: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-// TEST CASE 4
-const oneArray = tail(["hello"]);
-assertEqual(oneArray.length, 0);
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [6, 7, 8, 9] for [5, 6, 7, 8, 9]", () => {
+    assert.deepEqual(tail([5, 6, 7, 8, 9]), [6, 7, 8, 9]);
+  });
+  it("returns [] for ['Hello']", () => {
+    assert.deepEqual(tail(['Hello']), []);
+  });
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  it("the tail function did not modify the original array [1, 2, 3]", () => {
+    let array = [1, 2, 3];
+    tail(array);
+    assert.deepEqual(array, [1, 2, 3]);
+  });
+});
