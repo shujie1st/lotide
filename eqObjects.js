637b77e697e9c4a223647b5b42b6bc5462a5aad4
Shujie Ma <shujie1st@gmail.com>
2023-02-17 19:16:17 -0500

    Refactor code with Mocha and Chai

--- eqObjects.js
+++ eqObjects.js
@@ -1,4 +1,3 @@
-const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
 
 const eqObjects = function(object1, object2) {
@@ -34,50 +33,6 @@ const eqObjects = function(object1, object2) {
   }
   return true;
 };
-
-
-// TEST CODE
-const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject = { size: "medium", color: "red" };
-eqObjects(shirtObject, anotherShirtObject); // => true
-assertEqual(eqObjects(shirtObject, anotherShirtObject), true);
-
-
-const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
-eqObjects(shirtObject, longSleeveShirtObject); // => false
-assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
-
-const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
-const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
-eqObjects(multiColorShirtObject, anotherMultiColorShirtObject); // => true
-assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
-
-const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
-eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject); // => false
-assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
-
-const object01 = { a: { z: 1 }, b: null };
-const object02 = { a: { z: 1 }, b: null };
-eqObjects(object01, object02);
-assertEqual(eqObjects(object01, object02), true);
-
-
-const object03 = { a: { y: 0, z: 1 }, b: 2 };
-const object04 = { a: { z: 1 }, b: 2 };
-eqObjects(object03, object04);
-assertEqual(eqObjects(object03, object04), false);
-
-
-const object05 = { a: { y: 0, z: 1 }, b: 2 };
-const object06 = { a: { y: 0, z: 1 }, b: 2 };
-eqObjects(object05, object06);
-assertEqual(eqObjects(object05, object06), true);
-
-const object07 = { a: { 0: 1, 1: 2 }, b: 2 };
-const object08 = { a: [1, 2], b: 2 };
-eqObjects(object07, object08);
-assertEqual(eqObjects(object07, object08), false);
-
 
 
 module.exports = eqObjects;
---
+++ test/eqObjectsTest.js
@@ -0,0 +1,48 @@
+const assert = require('chai').assert;
+const eqObjects = require('../eqObjects');
+
+describe("#eqObjects", () => {
+  it("returns true for shirtObject and anotherShirtObject", () => {
+    const shirtObject = { color: "red", size: "medium" };
+    const anotherShirtObject = { size: "medium", color: "red" };
+    assert.strictEqual(eqObjects(shirtObject, anotherShirtObject), true);
+  });
+  it("returns false for shirtObject and longSleeveShirtObject", () => {
+    const shirtObject = { color: "red", size: "medium" };
+    const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+    assert.strictEqual(eqObjects(shirtObject, longSleeveShirtObject), false);
+  });
+  it("returns true for multiColorShirtObject and anotherMultiColorShirtObject", () => {
+    const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+    const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+    assert.strictEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true);
+  });
+  it("returns false for multiColorShirtObject and longSleeveMultiColorShirtObject", () => {
+    const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+    const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+    assert.strictEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false);
+  });
+  it("returns true for object01 and object02", () => {
+    const object01 = { a: { z: 1 }, b: null };
+    const object02 = { a: { z: 1 }, b: null };
+    assert.strictEqual(eqObjects(object01, object02), true);
+  });
+  it("returns false for object03 and object04", () => {
+    const object03 = { a: { y: 0, z: 1 }, b: 2 };
+    const object04 = { a: { z: 1 }, b: 2 };
+    assert.strictEqual(eqObjects(object03, object04), false);
+  });
+
+  it("returns true for object05 and object06", () => {
+    const object05 = { a: { y: 0, z: 1 }, b: 2 };
+    const object06 = { a: { y: 0, z: 1 }, b: 2 };
+    assert.strictEqual(eqObjects(object05, object06), true);
+  });
+  it("returns false for object07 and object08", () => {
+    const object07 = { a: { 0: 1, 1: 2 }, b: 2 };
+    const object08 = { a: [1, 2], b: 2 };
+    assert.strictEqual(eqObjects(object07, object08), false);
+  });
+});
+
+
