d7484f34fed24cc23ff1f7ce05d1e9d9298cfaf7
Shujie Ma <shujie1st@gmail.com>
2023-02-17 19:41:44 -0500

    Refactor code with Mocha and Chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,12 +1,38 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
-assertEqual(eqArrays([], []), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+describe("#eqArrays", () => {
+  it("returns true for [1, 2, 3] and [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  it("returns false for [[1, 2, 3] and [3, 2, 1]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+  it("returns true for ['1', '2', '3'] and ['1', '2', '3']", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', '3']), true);
+  });
+  it("returns false for ['1', '2', '3'] and ['1', '2', 3]", () => {
+    assert.strictEqual(eqArrays(['1', '2', '3'], ['1', '2', 3]), false);
+  });
+  it("returns false for [1, 2, 3] and [1, 2, 3, 4]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+  });
+  it("returns true for [] and []", () => {
+    assert.strictEqual(eqArrays([], []), true);
+  });
+  it("returns true for [[2, 3], [4]] and [[2, 3], [4]]", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it("returns false for [[2, 3], [4]] and [[2, 3], [4, 5]]", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+  it("returns false for [[2, 3], [4]] and [[2, 3], 4]", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
+  });
+  it("returns true for [[2, [4, 5], 3], [4]] and [[2, [4, 5], 3], [4]]", () => {
+    assert.strictEqual(eqArrays([[2, [4, 5], 3], [4]],[[2, [4, 5], 3], [4]]), true);
+  });
+  it("returns false for [[2, [4, 5, 6], 3], [4]] and [[2, [4, 5], 3], [4]]", () => {
+    assert.strictEqual(eqArrays([[2, [4, 5, 6], 3], [4]],[[2, [4, 5], 3], [4]]), false);
+  });
+});
